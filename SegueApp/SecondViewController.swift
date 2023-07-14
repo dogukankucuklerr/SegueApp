1d6c25c9911a18fd2850b799f30585c58b5a51ef
Doğukan Küçükler <dogukankucuklerr@gmail.com>
2023-07-14 23:37:58 +0300

    Initial Commit

---
+++ SegueApp/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  SegueApp
+//
+//  Created by Doğukan Küçükler on 22.11.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var myLabel2: UILabel!
+   
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var myName = ""
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLabel.text = myName
+        
+        
+        
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
